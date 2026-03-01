5da4a9224cfb666745afbf12daa944748d3d1c79
Eldasus <limonndar@gmail.com>
2026-03-01 13:29:44 +0100

    initial commit: hellocompose app

---
+++ app/src/main/java/com/example/hellocompose/MainActivity.kt
@@ -0,0 +1,35 @@
+package com.example.hellocompose
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.tooling.preview.Preview
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            HelloComposeApp()
+        }
+    }
+
+}
+@Composable
+fun HelloComposeApp() {
+    MaterialTheme {
+        var name by remember { mutableStateOf("Студент") }
+        Surface {
+            Column {
+                Text(text = "Привіт, $name!", style =
+                    MaterialTheme.typography.headlineMedium)
+                Button(onClick = { name = "Світ" }) { Text("Натисни") }
+            }
+        }
+    }
+}
+@Preview(showBackground = true)
+@Composable
+fun PreviewHello() {
+    HelloComposeApp()
+}
